a69ad0fb92ff6a87e9e03374815843134e4e10cc
Ranadeep Dutta <ranadeepdutta200@gmail.com>
2022-05-30 08:09:24 +0530

    second commit

---
+++ Project2.java
@@ -0,0 +1,11 @@
+
+
+public class Project2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World and Welcome to Jenkins");
+		System.out.println("let start with maven poject");
+
+	}
+
+}
